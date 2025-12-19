4b28ea1839b8dbc34fc0fbb83c9226a984052afd
paulosilvabr <pauloricardosousasilva10@gmail.com>
2025-12-19 00:27:41 -0300

    Update funcoes.js

--- ex/ex011/funcoes.js
+++ ex/ex011/funcoes.js
@@ -93,3 +93,133 @@ const numeros = [5, 12, 8, 130, 44];
 
 const maiorQueDez = numeros.filter(num => num > 10)
 console.log(maiorQueDez)
+
+console.log('<--------------------------------->')
+
+// Functions Expression
+console.log('Functions Expression');
+
+/*
+    function saldacao() {
+        console.log('Olá pessoa')    
+    }
+*/
+
+const saldacao = function() {
+    console.log('Olá pessoa')
+}
+
+saldacao()
+
+console.log('<--------------------------------->')
+
+// Outra forma de execultar uma função
+console.log('Outra forma de execultar uma função');
+
+function execFuncao(funcao) { // O parametro tem que ser um Function Expression
+    funcao()
+}
+
+execFuncao(saldacao); // Execultando a Função saldacao() dentro como parâmetro de execFuncao()
+
+console.log('<--------------------------------->')
+
+// Dentro de Objeto
+console.log('Dentro de Objeto');
+
+const obj = {
+    // falar: () => console.log('Falando abobrinha...')  <<--- tambem funciona
+    falar() {
+        console.log('Falando abobrinha...')
+    }
+}
+
+obj.falar()
+
+console.log('<--------------------------------->')
+
+// Arguments
+console.log('Arguments');
+
+function novosNumeros() {
+    console.log(arguments)
+}
+novosNumeros(0, 1, 2, 3, 4, 5, 6, 7, 8, 9) // Argumentos que eu mandei como parametros
+
+console.log('<--------------------------------->')
+
+// IIFE -> Immediately invoked function expression
+console.log('IIFE');
+/*
+    FEITA PARA FUGIR DAS VARIÁVEIS GLOBAIS, O QUE É UMA BOA PRÁTICA
+
+    SINTAXE:
+
+    (function(param){
+        const naoGlobal = 'Váriavel não global'
+        console.log(`${naoGlobal} e seu parametro ${param}`)
+    })(20)
+    
+    OBS: Pode ter ou não parâmetros
+*/
+
+(function(idade, peso, altura) {
+    const sobrenome = 'Ricardo';
+
+    function criaNome (nome) {
+        return nome + ' ' + sobrenome;
+    }
+
+    function falaNome() {
+        console.log(criaNome ('Paulo'));
+    }
+
+    falaNome();
+    console.log(idade, peso, altura);
+})(30, 80, 1.80);
+
+console.log('<--------------------------------->')
+
+// Factory Function
+console.log('Factory Function');
+// Constructor function (Função construtora)
+function criaPessoa(nome, sobrenome, a, p) {
+  return {
+    nome,
+    sobrenome,
+
+    // Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(' ');
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(' ');
+    },
+
+    fala(assunto = 'falando sobre NADA') {
+      return `${this.nome} está ${assunto}.`;
+    },
+
+    altura: a,
+    peso: p,
+
+    // Getter
+    get imc() {
+      const indice = this.peso / (this.altura ** 2);
+      return indice.toFixed(2);
+    }
+  };
+}
+
+const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80);
+const p2 = criaPessoa('João', 'Otávio', 1.90, 57);
+const p3 = criaPessoa('Junior', 'Otávio', 1.5, 110);
+
+console.log(p1.imc);
+console.log(p2.imc);
+console.log(p3.imc);
+
