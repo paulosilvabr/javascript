b672f0cba038b488b1362315714109cc52974da3
paulosilvabr <pauloricardosousasilva10@gmail.com>
2025-12-23 23:07:13 -0300

    forEach()

--- ex/ex010/array.js
+++ ex/ex010/array.js
@@ -1,4 +1,15 @@
-let num = [5 , 3 , 7 , 2 , 2]
+const num = [5 , 3 , 7 , 2 , 2]
+const pessoas = [
+    { nome: 'Patrícia', idade: 53 },
+    { nome: 'Bruno', idade: 18 },
+    { nome: 'Camila', idade: 29 },
+    { nome: 'Otávio', idade: 60 },
+    { nome: 'Beatriz', idade: 25 },
+    { nome: 'Sérgio', idade: 39 },
+    { nome: 'Helena', idade: 66 },
+    { nome: 'Igor', idade: 17 },
+    { nome: 'Mônica', idade: 41 },
+];
 
 console.log(num);
 console.log(`Posição 4: ${num[4]}`);
@@ -104,5 +115,72 @@ console.log(numFiltrado);
 // ---> Exemplo 3
 
 const numFiltrado = num.filter( valor => valor > 10)
+// Ele retorna valor, indice e array assim como map()
 
 console.log(numFiltrado);
+
+console.log('<-------------------------------->');
+
+console.log(num);
+
+// Exemplo 1
+const dobro = num.map((valor, indice, array) => {
+    console.log(`Esse é o ${valor}\nEsse é o indice do valor ${indice}\nE esse é a array completa ${array}\n`);
+})
+
+console.log(pessoas);
+
+// Exemplo 2
+const nomes = pessoas.map((valor, index, array) => valor.nome)
+console.log(nomes);
+
+// Exemplo 3
+const idades2 = pessoas.map((obj, index, array) => ({ idade: obj.idade}))
+console.log(idades2);
+
+// Exemplo 4
+/*
+const idades3 = pessoas.map((obj, index, array) => {   // Afeta a Array/Obj original 
+    delete obj.nome
+    return obj
+})
+console.log(idades3);
+*/
+
+// Exemplo 5
+const idParaPessoa = pessoas.map((valor, indice) => {   // Sem alterar o Obj Original
+    const newObj = {... valor} // Spread Expression: "..."
+    newObj.id = indice // Não tem id mas ele adiciona
+    return newObj
+})
+console.log(idParaPessoa);
+
+console.log('<-------------------------------->');
+
+console.log(num);
+
+let total = num.reduce(function(acumulador, valor, indice, array) {
+    // console.log(acumulador, valor, indice, array);
+    acumulador += valor
+    return acumulador;
+}, 100);
+console.log(total);
+
+console.log('<-------------------------------->');
+
+console.log(num);
+
+const pares = num
+.filter(valor => valor % 2 === 0)
+.map((valor) => valor * 2)
+.reduce((acumulador, valor, index, array) => acumulador += valor)
+
+console.log(pares);
+
+console.log('<-------------------------------->');
+
+console.log(num);
+
+num.forEach((valor, index, array) => {
+    console.log(total += valor);
+})
